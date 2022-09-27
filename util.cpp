738e194ae89d7bdfc5bbf8ea3b031ad07102e7c7
Erika Abumere <abumere@usc.edu>
2022-09-27 02:13:36 +0000

    Initial commit for HW2

--- util.cpp
+++ util.cpp
@@ -15,16 +15,46 @@ std::string convToLower(std::string src)
     to a set of words based on the criteria given in the assignment **/
 std::set<std::string> parseStringToWords(string rawWords)
 {
+	set<string> results;
 
+  rawWords = convToLower(rawWords);
+  rawWords = trim(rawWords);
+  rawWords = rawWords + " "; //adding space to the end of the word helps me retrieve the very last word
 
-
-
-
-
-
-
-
-
+  string word = "";
+  for (auto i : rawWords){
+      if (i == ' '){ //end of the word
+        if (word.length() >= 2){ //checking if word is >= 2 chars before adding
+  
+          string subWord = ""; 
+          for (auto o : word){ //checking if word has punctuation 
+              if (ispunct(o)) { //end of first half of the word containing puncuation 
+                if (subWord.length() >= 2) { //only add this half of the word if it is >= 2 chars
+                  results.insert(subWord);
+                  subWord = "";
+                }
+                else { //if subWord is not greater than 2 chars, it is not a valid keyword, reset
+                  subWord = "";
+                }
+              }
+              else { //no puncuation yet, keep adding chars
+              subWord = subWord + o;
+              }
+          }
+          if (subWord.length() >= 2){ //to add the last word to the set
+            results.insert(subWord);
+          }
+          word = "";
+        } 
+        else {
+          word = "";
+        }  
+      } // end of if that checks for space 
+      else { // there are more chars to be added to the word
+        word = word + i;
+      }
+  }
+return results;
 }
 
 /**************************************************
@@ -55,3 +85,4 @@ std::string &rtrim(std::string &s) {
 std::string &trim(std::string &s) {
     return ltrim(rtrim(s));
 }
+
--- util.h
+++ util.h
@@ -13,19 +13,124 @@
 template <typename T>
 std::set<T> setIntersection(std::set<T>& s1, std::set<T>& s2)
 {
+	//**************HAS RUNTIME REQUIREMENTS*******************
+  std::set<T> results;
+  std::set<T> biggerSet;
+  std::set<T> smallerSet;
 
-
-
-
+  //finding bigger & smaller set
+  if (s1.size() > s2.size()){ 
+    biggerSet = s1;
+    smallerSet = s2;
+  }else {
+    biggerSet = s2;
+    smallerSet = s1;
+  }
+  
+  for(auto i: biggerSet){
+    if( smallerSet.find(i) != smallerSet.end() )
+    {
+      results.insert(i);
+    }
+  }
+  return results;
 
 }
+
 template <typename T>
 std::set<T> setUnion(std::set<T>& s1, std::set<T>& s2)
 {
+		//*****************HAS RUNTIME REQUIREMENTS*********************
+  s1.insert(s2.begin(), s2.end());
+  return s1;
+
+}
+
+/***********************************************/
+/* Prototypes of functions defined in util.cpp */
+/***********************************************/
+
+std::string convToLower(std::string src);
+
+std::set<std::string> parseStringToWords(std::string line);
+
+// Used from http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+// Removes any leading whitespace
+std::string &ltrim(std::string &s) ;
+
+// Removes any trailing whitespace
+std::string &rtrim(std::string &s) ;
+
+// Removes leading and trailing whitespace
+std::string &trim(std::string &s) ;
+#endif
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <string>
+#include <iostream>
+#include <set>
 
 
+/** Complete the setIntersection and setUnion functions below
+ *  in this header file (since they are templates).
+ *  Both functions should run in time O(n*log(n)) and not O(n^2)
+ */
+template <typename T>
+std::set<T> setIntersection(std::set<T>& s1, std::set<T>& s2)
+{
+	//HAS RUNTIME REQUIREMENTS
+
+}
+
+template <typename T>
+std::set<T> setUnion(std::set<T>& s1, std::set<T>& s2)
+{
+		//HAS RUNTIME REQUIREMENTS
+
+}
+
+/***********************************************/
+/* Prototypes of functions defined in util.cpp */
+/***********************************************/
+
+std::string convToLower(std::string src);
+
+std::set<std::string> parseStringToWords(std::string line);
+
+// Used from http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+// Removes any leading whitespace
+std::string &ltrim(std::string &s) ;
+
+// Removes any trailing whitespace
+std::string &rtrim(std::string &s) ;
+
+// Removes leading and trailing whitespace
+std::string &trim(std::string &s) ;
+#endif
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <string>
+#include <iostream>
+#include <set>
 
 
+/** Complete the setIntersection and setUnion functions below
+ *  in this header file (since they are templates).
+ *  Both functions should run in time O(n*log(n)) and not O(n^2)
+ */
+template <typename T>
+std::set<T> setIntersection(std::set<T>& s1, std::set<T>& s2)
+{
+	//HAS RUNTIME REQUIREMENTS
+
+}
+
+template <typename T>
+std::set<T> setUnion(std::set<T>& s1, std::set<T>& s2)
+{
+		//HAS RUNTIME REQUIREMENTS
 
 }
 
